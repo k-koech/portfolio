c216f64c1bbd748dcde8fe84401d26e13e89ba9f
k-koech <kelvin.koech@student.moringaschool.com>
2021-06-06 18:52:30 +0300

    added a google map to the footer section

---
+++ script.js
@@ -0,0 +1,18 @@
+// My location map
+function initMap() {
+    // The location of Rongai
+    
+    const rongai = { lat: -1.2920658999999999, lng: 36.8219462 };
+    // The map, centered at Rongai
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: rongai,
+    });
+    // The marker, positioned at Rongai
+    const marker = new google.maps.Marker({
+      position: rongai,
+      map: map,
+    });
+  }
+
+  
